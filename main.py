ef49e9f3d84c6d1d72a297cadc264e5eb8eeac63
KarteekAti <87597184+KarteekAti@users.noreply.github.com>
2021-10-25 15:23:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask 
+from flask_socketio import SocketIO, send
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+@socketio.on('message')
+def handleMessage(msg):
+	print('Message: ' + msg)
+	send(msg, broadcast=True)
+
+if __name__ == '__main__':
+	socketio.run(app,debug=True,host='0.0.0.0')   
