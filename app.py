3cdbd139ee8246afec55e701e7ab0bf61d3ab375
karteek <ati.karteek99@gmail.com>
2021-10-26 11:03:36 +0530

    heroku update

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask 
 from flask_socketio import SocketIO, emit, send
-import config
 import os
+
+class Config:
+    TESTING = 'False'
+    DEBUG = 'True'
+    SECRET_KEY = os.environ.get('SECRET')
+    SERVER = '0.0.0.0'
+    
 app = Flask(__name__,instance_relative_config=False)
-app.config.from_object('config.Config')
+app.config.from_object('Config')
 socketio = SocketIO(app,cors_allowed_origins='*')
+
+@app.route('/')
 
 @socketio.on('message')
 def handleMessage(msg):
@@ -13,4 +21,4 @@ def handleMessage(msg):
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
-	socketio.run(app, host=(config.Config.SERVER),port=port)  
+	socketio.run(app, host=(Config.SERVER),port=port)  
--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    TESTING = 'False'
-    DEBUG = 'True'
-    SECRET_KEY = os.environ.get('SECRET')
-    SERVER = '0.0.0.0'
-    
