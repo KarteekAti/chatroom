99c3fb434f110392b255e8cc74b9dd598c1627ba
karteek <ati.karteek99@gmail.com>
2021-10-30 11:44:09 +0530

    2.0

--- static/index.js
+++ static/index.js
@@ -1,5 +1,4 @@
 var connectionOptions =  {
-    "force new connection" : true,
     "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
     "timeout" : 10000, //before connect_error and connect_timeout are emitted.
     "transports" : ["websocket"]
