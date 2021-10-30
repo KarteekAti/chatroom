17c6b9ac6a1f5e835467edb2b0e1a8fbccbd6a6a
karteek <ati.karteek99@gmail.com>
2021-10-30 11:46:32 +0530

    2.0

--- static/index.js
+++ static/index.js
@@ -1,12 +1,8 @@
-var connectionOptions =  {
-    "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
-    "timeout" : 10000, //before connect_error and connect_timeout are emitted.
-    "transports" : ["websocket"]
-};
+
 
 
 $(document).ready(function() {
-const socket = io("http://"+document.domain+":"+location.port,connectionOptions); 
+const socket = io("http://"+document.domain+":"+location.port+'/home'); 
 
 console.log(socket); 
 
